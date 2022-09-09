b563f8f14295453113f7a9467d0557599cf8a3e5
Nicolas Matheus Ferreira <71523671+NicolasMatheusFerreira@users.noreply.github.com>
2022-09-09 20:10:34 -0300

    Atualizando repositório

--- codigo/Lab2/Fibonacci.cs
+++ codigo/Lab2/Fibonacci.cs
@@ -11,7 +11,8 @@ class Fibonacci {
 
 	static void Main(string[] args) {
 
+		Console.WriteLine("Sequencia fibonacci.");
 		for(int i = 0; i<10; i++) 
-			Console.WriteLine(FibonacciRec(i));
+			Console.Write(FibonacciRec(i)+" ");
 	}
 }
