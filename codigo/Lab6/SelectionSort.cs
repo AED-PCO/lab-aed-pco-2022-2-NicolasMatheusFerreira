e7777c86eaec1add906ca19495dc9e6c2421ab72
Nicolas Matheus Ferreira <71523671+NicolasMatheusFerreira@users.noreply.github.com>
2022-10-12 14:25:52 -0300

    Adicionando relatórios lista 5

--- codigo/Lab6/SelectionSort.cs
+++ codigo/Lab6/SelectionSort.cs
@@ -3,53 +3,34 @@ using System.Diagnostics;
 
 class SelectionSort {
 
-	static void Ordenacao(int[] Vetor, char[] Carc) {
-		int menorInt = 0;
-
-		for(int i = 0; i<Vetor.Length; i++) {
-			menorInt = i;
-			for(int j = i; j<Vetor.Length; j++) {
-				if (Vetor[j]<Vetor[menorInt]) {
-					menorInt = j;
-				}
+	static void Ordenacao(int[] Vetor) {
+		int chave, j;
+		for(int i = 1; i<Vetor.Length; i++) {
+			chave = Vetor[i];
+			j = i - 1;
+			while(j>=0 && Vetor[j]>chave) {
+				Vetor[j+1] = Vetor[j];
+				j--;
+			
+				Vetor[j+1] = chave;
 			}
-			int aux = Vetor[i];
-			Vetor[i] = Vetor[menorInt];
-			Vetor[menorInt] = aux;
 		}
-		
-
-		int menorChar = 0;
-		for(int i = 0; i<Carc.Length; i++) {
-			menorChar = i;
-			for(int j = i; j<Carc.Length; j++) {
-				if (Carc[j]<Carc[menorChar])
-					menorChar = j;
-			}
-			char aux = Carc[i];
-			Carc[i] = Carc[menorChar];
-			Carc[menorChar] = aux;
-		}
-
+			
+		Console.WriteLine("Ordenado...!");
 	}
 	static void Main(string[] args) {
 
 		int[] Vetor = new int[] {45, 6, 12, 32, 2, 1, 67, 28, 5, 64};
-		char[] Carc = new char[] {'n', 'h', 'o', 'w', 'a', 'v', 'b', 's', 'c'};
 		var stopwatch = new Stopwatch();
 
 		stopwatch.Start();
-		Ordenacao(Vetor, Carc);
+		Ordenacao(Vetor);
 		stopwatch.Stop();
 
 		for(int i = 0; i<Vetor.Length; i++) {
 			Console.Write("{0} ", Vetor[i]);		
 		}
 		Console.WriteLine();
-		for(int i = 0; i<Carc.Length; i++) {
-			Console.Write("{0} ", Carc[i]);			
-		}
-		Console.WriteLine();	
 		Console.WriteLine("{0} Mil. Seg.", stopwatch.ElapsedMilliseconds);
 
 	}
